0b1e1489332d0874b459f00009da4b09b8183d8e
jimc <carstens.jim@gmail.com>
2019-04-04 00:32:36 +0200

    add write to file

--- week6_ex10_sudoku/SA_Sudoku_Jim/SA_Sudoku_Jim/main.cpp
+++ week6_ex10_sudoku/SA_Sudoku_Jim/SA_Sudoku_Jim/main.cpp
@@ -2,14 +2,15 @@
 
 #include "SudokuSolver.h"
 #include <iostream>
+#include <fstream>
 
 using std::cout; using std::endl;
 
 int main() {
 	int mc_steps = 500000;
 	int output_steps = 10000;
-	double TStart = 10.;
-	double alpha = 0.9;
+	double TStart = 4.;
+	double alpha = 0.93;
 
 	SudokuSolver su(9);
 	su.read("sudoku.dat");
@@ -22,25 +23,35 @@ int main() {
 	su.setTemperature(TStart);
 	double temperature = su.getTemperature();
 	cout << "E0 = " << E << ", T0 = " << temperature << endl;
+
+
+	std::ofstream myfile;
+	myfile.open("energy.dat");
+
 	while (E != 0) {
 		for (int i = 0; i < mc_steps; i++) {
 			su.randomChange();
 			E = su.calculateEnergy();
+			if (E == 0) {
+				break;
+			}
 			//su.print();			
 			if (i% output_steps == 0 && i != 0) {
 				temperature *= alpha;
 				su.setTemperature(temperature);
 			}
-			if (i % 1000 == 0) {
+			if (i % 10000 == 0) {
 				cout << "{" << temperature << ", " << E << "}, " << endl;
+				myfile << temperature << ',' << E << endl;
 			}			
 		}
 		su.fillRandom();
 		su.setTemperature(TStart);
 		temperature = su.getTemperature();
 	}
+	myfile << temperature << ',' << E << endl;
 	cout<<"E = " << E <<", T =  "<< temperature<<endl;
 	su.print();
-	
+	myfile.close();
 	return 0;
 }
