d9f4b923e08c415cbde7108052e3609b1454dd4b
jimc <carstens.jim@gmail.com>
2019-05-03 12:44:07 +0200

    start on Ising model first PYTHON
    Make a random model,
    add a python script that plots the configurations

---
+++ week8_ex2-3_Ising/Ising/ising_plot.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as pt
+import sys
+
+ftype = "conf"
+data = []
+dir = "configurations/"
+speed = 1000000
+
+##Initialisation
+file = open(dir + ftype + "_step0.csv", "r")
+for line in file:
+    data.append([])
+    line_list = list(line.split(","))
+    for l in line_list:
+        data[-1].append(float(l))
+file.close()
+
+#Plotting of the whole model
+def plotModel(input_data):
+    xlen, ylen = len(input_data), len(input_data[0])
+    spin_up_x, spin_up_y, spin_down_x, spin_down_y = [], [], [], []
+    for x in range(xlen):
+        for y in range(ylen):
+            if input_data[x][y] == 1:
+                spin_up_x.append(x)
+                spin_up_y.append(y)
+            elif input_data[x][y] == -1:
+                spin_down_x.append(x)
+                spin_down_y.append(y)
+            else:
+                print(input_data[x][y])
+
+def readCSV(filename):
+    file = open(filename, "r")
+    ld = 0
+    for line in file:
+        dt = list(line.split(","))
+        for l in range(len(dt)):
+            data[ld][l] = float(dt[l])
+        ld += 1
+    file.close()
+pt.hot()
+fig = pt.imshow(data,interpolation='none',vmin=-1.0,vmax=1.0)
+
+tot_steps = 10000
+step = 1
+for i in range(0, tot_steps, step):
+    try:
+        readCSV(dir + ftype + "_step{:d}.csv".format(i))
+    except:
+        print("Error: could not open data file: " + dir + ftype + "_step{:d}.csv".format(i))
+        exit()
+    plotModel(data)
+    fig.set_data(data)
+    pt.pause(1.E-6)
+    pt.draw()
--- week8_ex2-3_Ising/Ising/main.cc
+++ week8_ex2-3_Ising/Ising/main.cc
@@ -4,22 +4,24 @@
 #include <random>
 #include <chrono>
 using std::cout; using std::endl;
-
+#pragma warning(disable : 4996)
+#include <stdio.h>
 class Lattice {
 public:
 private:
 	int _lattice[];
 };
-const int N = 10;
-int lattice[N * N] = { 0. };
+const int N = 50;
+int lattice[N * N] = { 0 };
+
 void print() {
 	for (int i = 0; i < 10; i++) {
 		cout << "---";
 	}
 	cout << "\n";
 	for (int i = 0; i < 100; i++) {
-		cout<<std::showpos << lattice[i] << ", ";
-		if ((i+1) % 10 == 0) {
+		cout << std::showpos << lattice[i] << ", ";
+		if ((i + 1) % 10 == 0) {
 			cout << "\n";
 		}
 	}
@@ -27,6 +29,21 @@ void print() {
 		cout << "---";
 	}
 }
+
+void writeConfiguration(int step) {
+	char buffer[128];
+	sprintf(buffer, "configurations/conf_step%d.csv", step);
+	FILE* fp = fopen(buffer, "w");
+	for (int k = 0; k < N; k++) {
+		for (int l = 0; l < N - 1; l++) {
+			fprintf(fp, "%d,", lattice[k * N + l]);
+	
+		}
+		fprintf(fp, "%d\n", lattice[k * N + N - 1]);
+	}
+	fclose(fp);
+}
+
 int magnetisation() {
 	int M = 0;
 	for (int i = 0; i < N * N; i++) {
@@ -50,13 +67,16 @@ int main() {
 
 	print();
 	for (int i = 0; i < mc_steps; ++i) {
-		if (random_zero_one(rng) < 0.5) {
-			int random_site =  std::uniform_int_distribution<int>(0, N*N)(rng);
-			lattice[random_site] = -lattice[random_site];
+		if (random_zero_one(rng) < 0.1) {
+			int random_site = std::uniform_int_distribution<int>(0, N * N)(rng);
+			lattice[random_site] = -1;
 		}
 		//print();
-		cout << magnetisation()<<", \n";
+		cout << magnetisation() << ", \n";
+		writeConfiguration(i);
+		//write_data(i);
 	}
+
 
 	return 0;
 }
