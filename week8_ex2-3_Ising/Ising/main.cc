aeb8ad153fd7ab157d4e4e53d311991907964248
jimc <carstens.jim@gmail.com>
2019-05-10 16:41:55 +0200

    Update main.cc

--- week8_ex2-3_Ising/Ising/main.cc
+++ week8_ex2-3_Ising/Ising/main.cc
@@ -6,24 +6,68 @@
 #include <stdio.h>
 using std::cout; using std::endl;
 
+class Lattice {
+public:
+	Lattice(unsigned width, unsigned heigth) : _width(width), _heigth(heigth), _array(0) {
+		_size = width * heigth;
+		if (width > 0 && heigth > 0) {
+			_array = new int[_size];
+		}
+		init(1);
+	}
+	~Lattice() {
+		delete[] _array;
+	}
+	void init(int value) {
+		for (unsigned i = 0; i < _size; i++) {
+			_array[i] = value;
+		}
+	}
+	const int& operator()(int x, int y) const {
+		if (x >= _width || x<0 || y>_heigth || y < 0) { return 999; }
+		return _array[y * _width + x];
+	}
+	int& operator()(int x, int y) {
+		if (x >= _width || x < 0 || y >= _heigth || y < 0) { cout << "ERROR"; }
+		return _array[y * _width + x];
+	}
+	int& at(int i) {
+		if (i < 0 || i >= _size) { cout << "ERROR"; }
+		return _array[i];
+	}
+	void set(int x, int y, int value) {
+		if (x >= _width || x<0 || y>_heigth || y < 0) { cout << "ERROR"; }
+		_array[y * _width + x] = value;
+	}
+	// get dims
+	unsigned get_width() const { return _width; }
+	unsigned get_heigth() const { return _heigth; }
+
+	// private data members
+private:
+	unsigned _width, _heigth, _size;
+	int* _array;
+	// to prevent unwanted copying:
+	Lattice(const Lattice&);
+	Lattice& operator= (const Lattice&);
+};
+
 std::mt19937 rng;
 std::uniform_real_distribution<double> random_zero_one(0.0, 1.0);
 double beta, J;
-class Lattice {
-public:
-private:
-	int _lattice[];
-};
 const int N = 50;
-int lattice[N * N] = { 0 };
-double prob[5];
+const int lattice_size = N * N;
+//int lattice[N * N] = { 0 };
+double prob[8];
+Lattice lat(N, N);
+
 void print() {
 	for (int i = 0; i < N; i++) {
 		cout << "---";
 	}
 	cout << "\n";
 	for (int i = 0; i < N * N; i++) {
-		cout << std::showpos << lattice[i] << ", ";
+		cout << std::showpos << lat.at(i) << ", ";
 		if ((i + 1) % N == 0) {
 			cout << "\n";
 		}
@@ -40,9 +84,9 @@ void write_data(int step) {
 	file.open(buffer);
 	for (int k = 0; k < N; k++) {
 		for (int l = 0; l < N - 1; l++) {
-			file << lattice[k * N + l] << ',';
+			file << lat(k, l) << ',';
 		}
-		file << lattice[k * N + N - 1] << "\n";
+		file << lat(k, N - 1) << "\n";
 	}
 	file.close();
 }
@@ -51,7 +95,7 @@ void write_data(int step) {
 int magnetisation() {
 	int M = 0;
 	for (int i = 0; i < N * N; i++) {
-		M += lattice[i];
+		//	M += lattice[i];
 	}
 	return M;//per latice
 }
@@ -69,17 +113,30 @@ int magnetic_autocorrelation() {
 }
 
 void initialize() {
-	for (int i = 2; i < 5; i += 2) prob[i] = exp(-2 * beta * i);
+	for (int i = 2; i < 5; i += 2) {
+		prob[i] = exp(-2 * beta * i);
+	}
 }
-
-
+void init_infinite_temperature() {
+	for (int x = 0; x < N; x++) {
+		for (int y = 0; y < N; y++) {
+			if (random_zero_one(rng) <= 0.5) {
+				lat(x, y) = -1;
+			}
+			else {
+				lat(x, y) = 1;
+			}
+		}
+	}
+}
+/*
 void sweep() {
 	int i;
 	int nn, sum, delta;
 	for (int k = 0; k < N; k++) {
-		// Choose a site 
+		// Choose a site
 		i = std::uniform_int_distribution<int>(0, N * N)(rng);
-		// Calculate the sum of the neighbouring spins 
+		// Calculate the sum of the neighbouring spins
 		if ((nn = i + 1) >= N) nn -= N;
 		sum = lattice[nn];
 		if ((nn = i - 1) < 0) nn += N;
@@ -88,9 +145,9 @@ void sweep() {
 		sum += lattice[nn];
 		if ((nn = i - N) < 0) nn += N;
 		sum += lattice[nn];
-		// Calculate the change in energy 
+		// Calculate the change in energy
 		delta = sum * lattice[i];
-		//Decide whether to flip spin 
+		//Decide whether to flip spin
 		if (delta <= 0) {
 			lattice[i] = -lattice[i];
 		}
@@ -99,32 +156,41 @@ void sweep() {
 		}
 	}
 }
-
-
-int main() {
-	beta = -2.3;
-	double B = 0.;
-	std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-	const int mc_steps = 100000;
-	const int output_steps = 100;
-
-	for (int i = 0; i < N * N; i++) {
-		if (random_zero_one(rng) <= 0.5) {
-			lattice[i] = -1;
-		}
-		else {
-			lattice[i] = 1;
+*/
+void sweep2() {
+	for (int i = 0; i < lattice_size; i++) {
+		int random_x = std::uniform_int_distribution<int>(0, N)(rng);
+		int random_y = std::uniform_int_distribution<int>(0, N)(rng);
+		double delta_E = 0.;
+		for (int dx = -1; dx < 2; dx++) {
+			for (int dy = -1; dy < 2; dy++) {
+				if (!(dx == 0 && dy == 0)) {
+					delta_E += 2 * J * lat(random_x, random_y) * lat(random_x + dx, random_y + dy);
+					//periodic boundary conditions
+				}
+			}
 		}
 	}
-	print();
-	initialize();
-	for (int i = 0; i < mc_steps; ++i) {
-		sweep();
-		if (i % output_steps == 0) {
-			cout << magnetisation() << ", \n";
-			write_data(i);
-		}
-	}
-
-	return 0;
 }
+
+
+	int main() {
+		lat.init(1);
+		beta = 1 / 2.;
+		std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+		const int mc_steps = 100000;
+		const int output_steps = 100;
+
+
+		print();
+		initialize();
+		for (int i = 0; i < mc_steps; ++i) {
+			sweep2();
+			if (i % output_steps == 0) {
+				cout << magnetisation() << ", \n";
+				write_data(i);
+			}
+		}
+
+		return 0;
+	}
